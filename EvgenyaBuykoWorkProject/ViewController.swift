dfc25b7febb6a01a08ca5e8a8f573758b2e2d36c
Kaimov-i <117822275+Kaimov-i@users.noreply.github.com>
2025-08-10 09:26:01 +0300

    lesson_12

--- EvgenyaBuykoWorkProject/ViewController.swift
+++ EvgenyaBuykoWorkProject/ViewController.swift
@@ -9,14 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var helper = Helper()
+    private let helper = Helper()
+    private let textLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
         updateNumbers()
+        
+        setupLabel()
+        view.addSubview(textLabel)
     }
     
+    private func setupLabel() {
+        let firstNumber = helper.getNumbers().first ?? 0
+        
+        let atributeString = NSMutableAttributedString(string: firstNumber.formatted())
+        atributeString.addAttributes([
+            .font: UIFont.systemFont(ofSize: 30, weight: .bold),
+            .foregroundColor: UIColor.red], range: NSRange(location: 0, length: 0))
+        textLabel.attributedText = atributeString
+        textLabel.frame = CGRect(x: 30, y: 30, width: 100, height: 50)
+    }
     private func updateNumbers() {
         helper.addNumer(Int.random(in: 1...10))
         
