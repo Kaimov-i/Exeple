cc8337d1346639ccfec374238a02d437b17bca24
Kaimov-i <117822275+Kaimov-i@users.noreply.github.com>
2025-08-13 19:40:20 +0300

    Lesson 13

--- EvgenyaBuykoWorkProject/ViewController.swift
+++ EvgenyaBuykoWorkProject/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     private let helper = Helper()
     private let textLabel = UILabel()
+    private let imageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +19,10 @@ class ViewController: UIViewController {
         updateNumbers()
         
         setupLabel()
+        setupImgae()
+        setupView()
         view.addSubview(textLabel)
+        view.addSubview(imageView)
     }
     
     private func setupLabel() {
@@ -31,6 +35,24 @@ class ViewController: UIViewController {
         textLabel.attributedText = atributeString
         textLabel.frame = CGRect(x: 30, y: 30, width: 100, height: 50)
     }
+    
+    private func setupImgae() {
+        imageView.image = UIImage(named: "priroda")
+        imageView.frame = CGRect(x: 30, y: 130, width: 100, height: 200)
+        imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
+        
+    }
+    private func setupView() {
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.green.cgColor, UIColor.blue.cgColor]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        
+        view.layer.insertSublayer(gradient, at: 0)
+    }
+    
     private func updateNumbers() {
         helper.addNumer(Int.random(in: 1...10))
         
